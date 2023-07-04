2824ea52a1614fceb2e02c34669545bc60f83b72
GottaGinger <114538296+GottaGinger@users.noreply.github.com>
2023-07-03 21:59:10 -0400

    commit

---
+++ CoffeeShopRegister/CoffeeShopRegister/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using CoffeeShopRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopRegister.Controllers
+{
+    public class UserController : Controller
+    {
+        private List<User> userInfo = new List<User> { };
+
+        public IActionResult Index()
+        {
+            return View(userInfo);
+        }
+
+        public IActionResult Summary(User user)
+        {
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user); 
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            userInfo.Add(user);
+            return RedirectToAction("Summary", user);
+        }
+    }
+}
